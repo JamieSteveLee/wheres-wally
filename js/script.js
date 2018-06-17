363daf40d45007fa7fc33a9dd1f6d481da201a30
Jamie Lee <jamie.lee@waterstones.com>
2018-06-17 20:56:56 +0100

    Added levels and other big changes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ var vm = new Vue({
 	el: '#vm',
 	data: {
 		currentLevel: 0,
+		showMenu: false,
+		showModal: false,
+		showFoundMsg: false,
+		latestFound: 'Wally',
 		levelData: [
 			{
 				class: 'lvl-01',
+				name: 'Level 1',
 				unlocked: true,
-				imgSrc: 'images/wheres-wally.jpg',
+				imgSrc: 'images/lvl-01.jpg',
 				found: {
 					wally: false,
 					woof: false,
@@ -17,8 +22,9 @@ var vm = new Vue({
 			},
 			{
 				class: 'lvl-02',
-				unlocked: false,
-				imgSrc: 'images/wheres-wally.jpg',
+				name: 'Level 2',
+				unlocked: true,
+				imgSrc: 'images/lvl-02.jpg',
 				found: {
 					wally: false,
 					woof: false,
@@ -29,8 +35,35 @@ var vm = new Vue({
 			},
 			{
 				class: 'lvl-03',
-				unlocked: false,
-				imgSrc: 'images/wheres-wally.jpg',
+				name: 'Level 3',
+				unlocked: true,
+				imgSrc: 'images/lvl-03.jpg',
+				found: {
+					wally: false,
+					woof: false,
+					wenda: false,
+					whitebeard: false,
+					odlaw: false
+				}
+			},
+			{
+				class: 'lvl-04',
+				name: 'Level 4',
+				unlocked: true,
+				imgSrc: 'images/lvl-04.jpg',
+				found: {
+					wally: false,
+					woof: false,
+					wenda: false,
+					whitebeard: false,
+					odlaw: false
+				}
+			},
+			{
+				class: 'lvl-05',
+				name: 'Level 5',
+				unlocked: true,
+				imgSrc: 'images/lvl-05.jpg',
 				found: {
 					wally: false,
 					woof: false,
@@ -44,42 +77,75 @@ var vm = new Vue({
 	computed: {
 		currentData: function() {
 			return this.levelData[this.currentLevel];
+		},
+		lastLevel: function() {
+			if(this.currentLevel == this.levelData.length - 1) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		nxtLvl: function() {
+			return this.currentLevel + 1;
 		}
 	},
 	methods: {
 		find: function(name) {
+			if(name == 'wally' && !this.levelData[this.currentLevel].found[name]) {
+				this.displayModal(true);
+				if(!this.lastLevel) {
+					this.levelData[this.nxtLvl].unlocked = true;
+				}
+			} else {
+				this.displayFoundMsg(name);
+			}
 			this.levelData[this.currentLevel].found[name] = true;
-
-			var nextLvl = this.currentLevel + 1;
-			if(name == 'wally' && nextLvl < this.levelData.length) {
-				this.levelData[nextLvl].unlocked = true;
-				console.log('Next level unlocked!');
+			// switch(name) {
+			// 	case "wally":
+			// 		alert("Oooh look you gone and found Wally didn't ya");
+			// 		break;
+			// 	case "whitebeard":
+			// 		alert("Hey its Wizard Whitebeard look at him the wise old man with the big stick and the big white beard");
+			// 		break;
+			// 	case "odlaw":
+			// 		alert("Nah that's not Wally thats yellow Wally thats the bad Wally");
+			// 		break;
+			// 	case "wenda":
+			// 		alert("hehehe its girl Wally");
+			// 		break;
+			// 	case "woof":
+			// 		alert("Woof woof woof you found the dog, but its only his tail but you got him well done");
+			// 		break;
+			// 	case "extra":
+			// 		alert("He's like the cat what got the cream");
+			// 		break;
+			// 	default:
+			// 		alert(name + " found");
+			// 		break;
+			// }
+		},
+		displayMenu: function(menuOpen) {
+			this.showMenu = menuOpen;
+		},
+		displayModal: function(modalOpen) {
+			this.showModal = modalOpen;
+		},
+		displayFoundMsg: function(charName) {
+			this.latestFound = charName;
+			this.showFoundMsg = true;
+			setTimeout(function() {
+				vm.showFoundMsg = false;
+			}, 2000);
+		},
+		selectLevel: function(thisLevel) {
+			if(this.levelData[thisLevel].unlocked) {
+				this.currentLevel = thisLevel;
+				this.displayMenu(false);
+				this.displayModal(false);
 			}
-
-			switch(name) {
-				case "wally":
-					alert("Oooh look you gone and found Wally didn't ya");
-					break;
-				case "whitebeard":
-					alert("Hey its Wizard Whitebeard look at him the wise old man with the big stick and the big white beard");
-					break;
-				case "odlaw":
-					alert("Nah that's not Wally thats yellow Wally thats the bad Wally");
-					break;
-				case "wenda":
-					alert("hehehe its girl Wally");
-					break;
-				case "woof":
-					alert("Woof woof woof you found the dog, but its only his tail but you got him well done");
-					break;
-				case "tubman":
-					alert("He's like the cat what got the cream");
-					break;
-				default:
-					alert(name + " found");
-					break;
-			}
-
+		},
+		goToNextLvl: function() {
+			this.selectLevel(this.nxtLvl);
 		}
 	},
 	beforeMount() {}
